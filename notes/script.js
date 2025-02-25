a583b46ee79746912cac16aeaa2a6ee1c56f9590
aashin12 <aashinkb369@gmail.com>
2025-02-25 22:23:57 +0530

    Notes of js

---
+++ notes/script.js
@@ -0,0 +1,53 @@
+var names='amal'
+console.log(names)
+
+var empName="kavya"
+console.log(empName)
+
+var studname= `Anu`
+console.log(studname)
+
+console.log(typeof(studname))
+
+/////////
+
+var n1 = 3
+var n2 = 3.33
+console.log(n1,n2)
+console.log(typeof(n2))
+
+const ab=true
+const bc= false
+console.log(ab)
+console.log(bc)
+
+///
+// undefined
+let name;
+console.log(name)
+
+let lastname=null
+console.log(lastname)
+
+//symbol
+
+c="hello"
+d="hello"
+console.log(c==d)
+
+var a = Symbol("hai")
+console.log(a)
+var b = Symbol("hai")
+console.log(a==b)
+
+//concationation
+
+let companyName = 'luminar technolab'
+let place = 'Kochi'
+
+console.log("My company name is ",companyName,"and Located in",place)
+console.log("My company name is "+companyName+"and Located in"+place)
+
+//interpolation
+
+console.log(`My company name is ${companyName} and Located in ${place}`)
